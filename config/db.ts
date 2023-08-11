c3ca923af70f10f6c98f3d5e76ca4998044c6c11
BrianSayen <blu3fire89@gmail.com>
2023-08-11 14:56:59 +0800

    updatE

--- config/db.ts
+++ config/db.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-const COLLECTION = "socket.io-adapter-events";
+const COLLECTION = 'socket.io-adapter-events';
 
 export const connectDB = async () => {
   if (process.env.MONGODB_URI) {
@@ -8,22 +8,18 @@ export const connectDB = async () => {
     console.log(`MongoDB is Connected`);
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-    createAdapterCol(db)
-    return db
+    createAdapterCol(db);
+    return db;
   } else {
     console.log('Unable to connect to datasbase');
   }
 };
 
 const createAdapterCol = async (conn: any) => {
-  console.log('create')
   try {
     await conn.createCollection(COLLECTION, {
       capped: true,
-      size: 1e6
+      size: 1e6,
     });
-  } catch (e) {
-    // console.log('e', e)
-    // collection already exists
-  }
-}
+  } catch (e) {}
+};
